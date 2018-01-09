c5ee3d2e44cd9b820d375867bb21f4251e6e4138
Sathvik Kuthuru <31018656+SathvikKuthuru@users.noreply.github.com>
2018-01-09 13:46:14 -0500

    Update TicTacToe.java

--- TicTacToe.java
+++ TicTacToe.java
@@ -16,7 +16,7 @@ public class TicTacToe {
     while (winner == 0){
       displayBoard(board);
       if (turn % 2 == 0) System.out.println("X you're up");
-      else System.out.println("It is Y's turn!");
+      else System.out.println("It is O's turn!");
       System.out.println("Where do you want to go");
       int x = scan.nextInt()-1;
       int y = scan.nextInt()-1;
@@ -30,9 +30,19 @@ public class TicTacToe {
         winner = 3;
       }
     }
+    
+    if(winner == 1) System.out.println("X is the Champ");
+    else if (winner = 2) System.out.println("O blew you out of the water!");
+    else System.out.println("Dencker just wins because he wrote me.");
   }
   
-  public static int checkWinner(char[][] board) {
+  public static int checkWinner(char[][] b) {
+    for(int i = 0; i < 3; i++) {
+      if(b[i][0] == 'X' && b[i][1] == 'X' && b[i][2] == 'X') return 1;
+      if(b[0][i] == 'X' && b[1][i] == 'X' && b[2][i] == 'X') return 1;
+      if(b[i][0] == 'O' && b[i][1] == 'O' && b[i][2] == 'O') return 2;
+      if(b[0][i] == 'O' && b[1][i] == 'O' && b[2][i] == 'O') return 2;
+    }
     return 0;
   }
   
